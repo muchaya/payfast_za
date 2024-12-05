89d6e48e12b022e295151e219edb9d45d2ec238f
Blessing Muchaya <iblessing@outlook.com>
2024-12-05 19:09:49 +0200

    require rails

--- lib/payfast_za.rb
+++ lib/payfast_za.rb
@@ -1,3 +1,4 @@
+require 'rails'
 require "payfast/attributes"
 require "payfast/client"
 require "payfast/config"
@@ -5,3 +6,4 @@ require "payfast/payment_builder"
 require "payfast/payment_methods"
 require "payfast/payment"
 require "payfast/signature_generator"
+ 
